4476ed7de47166f968cafaa7486a2b4c81595568
PercMain <bl9131@mac.com>
2017-03-11 15:38:34 -0600

    Initial Commit

---
+++ Voronoi_Fortunes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Voronoi_Fortunes
+//
+//  Created by Ben Lewson on 3/11/17.
+//  Copyright © 2017 Ben Lewson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
